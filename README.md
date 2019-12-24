e8525496c544203d67f26958647f0ebdb81cde1f
Austen Cameron <austen.cameron@gmail.com>
2019-12-24 14:49:05 -0700

    added README

---
+++ README.md
@@ -0,0 +1,8 @@
+# statamic-preset
+Scaffold a Statamic website juust right
+
+
+## Installation
+- First, create [a new Statamic project](https://statamic.dev/installation)
+- Require the preset `composer require austenc/statamic-preset`
+- Run the preset `php artisan preset statamic`
--- src/PresetServiceProvider.php
+++ src/PresetServiceProvider.php
@@ -9,10 +9,14 @@ class PresetServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        PresetCommand::macro('austencam-statamic', function ($command) {
-            Preset::install();
-            $command->info('Your preset has been installed successfully.');
-            $command->info('Please run `npm i && npm run dev` to compile assets.');
-        });
+        PresetCommand::macro('statamic', self::statamic());
+        PresetCommand::macro('statamic-preset', self::statamic());
+    }
+
+    public function statamic()
+    {
+        Preset::install();
+        $command->info('Your preset has been installed successfully.');
+        $command->info('Please run `npm i && npm run dev` to compile assets.');
     }
 }
